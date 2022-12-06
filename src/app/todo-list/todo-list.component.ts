e590e141551f5503ea328169313356a06a64e791
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-06 09:24:48 +0100

    basic logic woerking on todo-list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from '../item';
+import { Item } from '../interfaces/item';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,34 +7,30 @@ import { Item } from '../item';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-  filter: 'all' | 'active' | 'done' = 'all';
-  public allItems: Item[] = [
-    {
-      text: 'Etudier Angular',
-      done: false,
-    },
-    {
-      text: 'Faire du JavaScript',
-      done: false,
-    },
-  ];
+  public allItems: string[] = ['Learn JS'];
+
+  public newTask: string;
   constructor() {}
 
   ngOnInit(): void {}
 
-  getItems(): Item[] {
-    if (this.filter === 'all') {
-      return this.allItems;
+  addItem() {
+    if (this.newTask === '') {
+    } else {
+      this.allItems.unshift(this.newTask);
+      this.newTask = '';
     }
-    return this.allItems.filter((item) =>
-      this.filter === 'done' ? item.done : !item.done
-    );
   }
 
-  addItem(text: string) {
-    this.allItems.unshift({
-      text,
-      done: false,
-    });
+  deleteTask(index: number) {
+    this.allItems.splice(index, 1);
   }
+
+  /*  getItems(): [] {
+    return this.allItems.filter((item) => (item.done ? item.done : !item.done));
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }*/
 }
